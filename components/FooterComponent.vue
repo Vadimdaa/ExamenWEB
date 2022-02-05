4d783c3dad2f2c03f95efad77f26490549eaf2f5
Vadimdaa <71318302+Vadimdaa@users.noreply.github.com>
2022-02-05 15:27:34 +0300

    Экз

---
+++ components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>Вадим Мироненко</footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+}
+</script>
+
+<style>
+footer {
+  background: black;
+  color: white;
+  padding: 7px;
+  text-align: center;
+}
+</style>
